046e6aa319fb1c1d3025d6833616b1e35a7bf34b
Sol Manini <68747891+solmanini@users.noreply.github.com>
2024-09-06 13:33:11 +0200

    trigger new depliy

--- pages/api/send-email.tsx
+++ pages/api/send-email.tsx
@@ -33,6 +33,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(500).json({ error: 'Error sending email', details: error.message });
     }
   } else {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method not allowed..' });
   }
 }
