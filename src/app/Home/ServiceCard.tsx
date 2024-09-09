76422bb0c3fcf91d13e03798f4a0d3c3bd2f863d
Sol Manini <68747891+solmanini@users.noreply.github.com>
2024-09-09 19:34:11 +0200

    fix servuce tap

--- src/app/Home/ServiceCard.tsx
+++ src/app/Home/ServiceCard.tsx
@@ -8,16 +8,17 @@ interface ServiceCardProps {
   onSelect: () => void;
 }
 
-const handleServiceCardClick = (title: string) => {
+const handleServiceCardClick = (title: string, onSelect: () => void) => {
   trackEvent('button_click', {
     button_name: `Service Card ${title}`,
     section: 'Services'
   });
+  onSelect();
 };
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, isSelected, onSelect }) => {
   return (
-    <button onClick={() => handleServiceCardClick(title)} className="w-full group">
+    <button onClick={() => handleServiceCardClick(title, onSelect)} className="w-full group">
       <div 
         className={` rounded-xl p-4 md:px-6 flex-1 transition-all duration-300 ease-in-out overflow-hidden text-left 
         ${isSelected ? 'h-[220px] md:h-[180px] bg-white' : 'h-[60px] bg-dark-blue'}`}
